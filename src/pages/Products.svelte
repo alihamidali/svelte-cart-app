da8ac0d8e1eef93c0775f00a114b4f4f5adcd84f
Hamid Ali <alihamidali2@gmail.com>
2025-05-07 12:57:43 +0500

    show alert if something wrong with API or when product added to cart

--- src/pages/Products.svelte
+++ src/pages/Products.svelte
@@ -5,17 +5,24 @@
   let products = [];
   let cart = [];
   let loading = true;
+  let showAlert = false;
 
   onMount(async () => {
     const res = await fetch('https://fakestoreapi.com/products');
+    if (res.status !== 200) {
+      alert('Something went wrong with products API. Try again later!');
+    }
     const data = await res.json();
     await new Promise((resolve) => setTimeout(resolve, 1500));
     products = data;
     loading = false;
   });
 
-  const addToCart = (product) => {
+  const addToCart = async (product) => {
     cart = [...cart, product];
+    showAlert = true
+    await new Promise((resolve) => setTimeout(resolve, 800));
+    showAlert = false
   };
 
   const goToHomeWithCart = () => {
@@ -27,17 +34,19 @@
 {#if loading}
   <p class="text-center text-gray-600 italic">Loading products...</p>
 {:else}
+  <div class="mb-4 flex justify-between">
+    <button
+            class="bg-gray-500 hover:bg-gray-600 text-white px-5 py-2 rounded shadow transition cursor-pointer"
+            on:click={goToHomeWithCart}>
+      Go Back
+    </button>
+    {#if showAlert}
+      <h2 class="text-2xl font-bold text-green-700 mb-4">✅ Product Added to Cart!</h2>
+    {/if}
+  </div>
   <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-6">
     {#each products as product}
       <ProductCard {product} onAdd={addToCart} />
     {/each}
-  </div>
-
-  <div class="text-center">
-    <button
-      class="bg-gray-500 hover:bg-gray-600 text-white px-5 py-2 rounded shadow transition"
-      on:click={goToHomeWithCart}>
-      Go Back
-    </button>
   </div>
 {/if}
